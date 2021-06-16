c43d1e639c1f2a8ec7fa2abead4dfe34ab40d41a
Utkarsh Gaur <charmedtobe@gmail.com>
2021-06-15 22:42:44 -0400

    added hotkeys for ALL contorls

--- scripts/module.js
+++ scripts/module.js
@@ -11,19 +11,34 @@ Hooks.once('ready', async function() {
 		return;
 	}
     
-    console.log("Successfully Loaded Dragon Flagon's Brew")
+    console.log("UI-Hotkeys | Successfully Loaded Dragon Flagon's hotkeys library")
 
 	// Perform your Hotkey registrations
-    /* Hotkeys.registerShortcut(config: HotkeySetting): void */
-	
-	const TOKEN_TOOL = 'token';
 
-	// missing info in the readme w.r.t. js 
-	game.settings.register("UI-Hotkeys", TOKEN_TOOL, {
+	controls = [
+		{ control: "token", key: Hotkeys.keys.KeyT},
+		{ control: "measure", key: Hotkeys.keys.KeyM},
+		{ control: "tiles", key: Hotkeys.keys.KeyI},
+		{ control: "drawings", key: Hotkeys.keys.KeyD},
+		{ control: "walls", key: Hotkeys.keys.KeyW},
+		{ control: "lighting", key: Hotkeys.keys.KeyL},
+		{ control: "sounds", key: Hotkeys.keys.KeyA},
+		{ control: "notes", key: Hotkeys.keys.KeyN},
+	]
+
+	//registerKeyAndShortcut( { control: "measure", key: Hotkeys.keys.KeyM } );
+	controls.forEach(registerKeyAndShortcut)
+
+	console.log("UI-Hotkeys | Completed Hotkey Registration for controls UI")
+
+});
+
+function registerKeyAndShortcut( { control, key } ) {
+	game.settings.register("UI-Hotkeys", control, {
 		scope: 'world',
 		config: false,
 		default: {
-			key: Hotkeys.keys.KeyQ,
+			key: key,
 			alt: false,
 			ctrl: false,
 			shift: false
@@ -31,13 +46,11 @@ Hooks.once('ready', async function() {
 	});
 
 	Hotkeys.registerShortcut({
-		name: 'UI-Hotkeys.token', // <- Must be unique
-		label: 'UI-Hotkeys.token',
-		get: () => game.settings.get('UI-Hotkeys', `${TOKEN_TOOL}`),
-		set: async value => await game.settings.set('UI-Hotkeys', `${TOKEN_TOOL}`, value),
-		default: () => { return { key: Hotkeys.keys.KeyQ, alt: false, ctrl: false, shift: false }; },
-		onKeyDown: self => { ui.controls._onClickLayer({ preventDefault: () => { }, currentTarget: { dataset: { control: "token" } } }) },
+		name: `UI-Hotkeys.${control}`, // <- Must be unique
+		label: `Select ${control} controls`,
+		get: () => game.settings.get('UI-Hotkeys', `${control}`),
+		set: async value => await game.settings.set('UI-Hotkeys', `${control}`, value),
+		default: () => { return { key: key, alt: false, ctrl: false, shift: false }; },
+		onKeyDown: self => { ui.controls._onClickLayer({ preventDefault: () => { }, currentTarget: { dataset: { control: control } } }) },
 	});
-
-
-});
+}
